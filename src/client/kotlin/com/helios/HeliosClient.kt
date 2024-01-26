4cbd293c637d3e4a391debba45b11ea3648b10f4
choroalp <choroalp@skiff.com>
2024-01-26 23:29:39 +0300

    Init

---
+++ src/client/kotlin/com/helios/HeliosClient.kt
@@ -0,0 +1,9 @@
+package com.helios
+
+import net.fabricmc.api.ClientModInitializer
+
+object HeliosClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/helios/Helios.kt
@@ -0,0 +1,15 @@
+package com.helios
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object Helios : ModInitializer {
+    private val logger = LoggerFactory.getLogger("helios")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
